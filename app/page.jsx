668564e3e926e62fa74df3d516d2781ca334b05b
Dong-Han-Kim <dh32110@gmail.com>
2023-11-01 23:05:19 +0900

    useEffect 알고 쓰자

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react';
 import { Nav } from './components/Nav';
-import Image from 'next/image';
 
       {/* 만들어야 할 것
       1.nav
@@ -11,37 +10,41 @@ import Image from 'next/image';
 export default function Home() {
   const [movies, setMovies] = useState([]);
   
-  useEffect(() => {
-    const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: API_TOKEN
-      }
-    };
-    const moviesApi = async () => {
-      const json = await (
-        await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=ko-ko&page=1&sort_by=popularity.desc', options)
-      ).json()
-      console.log(json);
-      setMovies(json);
-      if(json){
-        console.log(json);
-      }
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: process.env.NEXT_PUBLIC_API_KEY
     }
-    moviesApi();
+  };
+  
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/4/account/6540ac1f4557a0013ac0020d/movie/recommendations?page=1&language=ko-ko', options)
+    .then(response => response.json())
+    .then((response) => {
+      console.log(response)
+      setMovies(response.results);
+    })
+    .catch(err => console.error(err));
+  console.log(movies)
   }, [])
-
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-0">
       {/* navigation */}
       <Nav />
       {/* header */}
-      <h1>{movies.results[0].title}</h1>
-      
+      {movies.map((movie, index) => {
+        return (
+          <>
+            <h1 key={index}>123</h1>
+            <h1>{movie.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="poster" />
+          </>
+        
+          )
+        })}
       {/* contents */}
-
     </main>
   )
 }
