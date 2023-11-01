d4dc36f722e9a9ecad145cef05e9b62eab31e5fb
Dong-Han-Kim <dh32110@gmail.com>
2023-11-01 10:35:42 +0900

    .env

--- app/page.jsx
+++ app/page.jsx
@@ -2,35 +2,41 @@
 import { useEffect, useState } from 'react';
 import { Nav } from './components/Nav';
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YjQwNmUwYjQ4ZWYzOGE5NjU0MzllOGQ3MGYyMmZlZCIsInN1YiI6IjY1NDBhYzFmNDU1N2EwMDEzYWMwMDIwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eLhE6jFhC8QqdghjVsHvegoQrb-mSUxdqDQrVOYaBNU'
-    }
-  };
-  
-  fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=ko-ko&page=1&sort_by=popularity.desc', options)
-    .then(response => response.json())
-    .then((response) => {
-      setMovies(response);
-      console.log(response)
-    })
-    .catch(err => console.error(err));
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-0">
       {/* 만들어야 할 것
       1.nav
       2.header
       3.main -> slide */}
+
+export default function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const API_TOKEN = process.env.NEXT_PUBLIC_apiToken;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: API_TOKEN
+      }
+    };
+    
+    fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=ko-ko&page=1&sort_by=popularity.desc', options)
+      .then(response => response.json())
+      .then((data) => 
+        setMovies(data)
+      )
+      .catch(err => console.error(err));
+}, [])
+    console.log(movies);
+// console.log(movies.results[0]);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between py-0">
       {/* navigation */}
       <Nav />
       {/* header */}
+
       {/* contents */}
+      {/* <div>{movies.results[0].title}</div> */}
     </main>
   )
 }
