da6156b34346a9eaf92456655fcd18bc8b6a387b
Dong-Han-Kim <dh32110@gmail.com>
2023-11-01 16:45:11 +0900

    error...

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react';
 import { Nav } from './components/Nav';
+import Image from 'next/image';
 
       {/* 만들어야 할 것
       1.nav
@@ -9,9 +10,9 @@ import { Nav } from './components/Nav';
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
-
+  
   useEffect(() => {
-    const API_TOKEN = process.env.NEXT_PUBLIC_apiToken;
+    const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
     const options = {
       method: 'GET',
       headers: {
@@ -19,24 +20,28 @@ export default function Home() {
         Authorization: API_TOKEN
       }
     };
-    
-    fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=ko-ko&page=1&sort_by=popularity.desc', options)
-      .then(response => response.json())
-      .then((data) => 
-        setMovies(data)
-      )
-      .catch(err => console.error(err));
-}, [])
-    console.log(movies);
-// console.log(movies.results[0]);
+    const moviesApi = async () => {
+      const json = await (
+        await fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=true&language=ko-ko&page=1&sort_by=popularity.desc', options)
+      ).json()
+      console.log(json);
+      setMovies(json);
+      if(json){
+        console.log(json);
+      }
+    }
+    moviesApi();
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-0">
       {/* navigation */}
       <Nav />
       {/* header */}
-
+      <h1>{movies.results[0].title}</h1>
+      
       {/* contents */}
-      {/* <div>{movies.results[0].title}</div> */}
+
     </main>
   )
 }
