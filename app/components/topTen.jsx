2894f762cd7a18e0b37097b8f162d0e81e82f096
Dong-Han-Kim <dh32110@gmail.com>
2023-11-17 22:31:29 +0900

    수정

--- app/components/topTen.jsx
+++ app/components/topTen.jsx
@@ -21,19 +21,23 @@ export default function TopTen ({movies}){
             {movies
               .map((movie) => {
                 return(
-                  <Link key={movie.id} href={`detail/${movie.id}`} className='mr-36'>
-                    <div className='w-36 mb-15 mr-15'>
-                      <div></div>
+                  <Link key={movie.id} href={`detail/${movie.id}`} className='mr-12'>
+                    <div className='flex w-full mb-20 mr-15'>
                       <img
                         src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
                         alt="Image"
+                        className='w-36'
                       />
+                      <div className='w-36 flex flex-col ml-3 bg-gray-600/50 p-3 rounded-md'>
+                        <h3 className='border-b-2 p-2 text-white text-center mb-3 font-extrabold'>{movie.title}</h3>
+                        <p className='line-clamp-6'>{movie.overview}</p>
+                      </div>
                     </div>
                   </Link>
                 )
               })
               .sort((a, b) => b.popularity - a.popularity)
-              .slice(0, 5)
+              .slice(0, 8)
             }
           </div>
         </div>
