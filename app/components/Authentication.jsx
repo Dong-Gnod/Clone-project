f382160ac765c23612ed9a8bdc98a971189622ad
Dong-Han-Kim <dh32110@gmail.com>
2023-11-21 18:36:57 +0900

    로그인이 유지되는 현상 해결

--- app/components/Authentication.jsx
+++ app/components/Authentication.jsx
@@ -1,14 +1,14 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react';
-import { useAuth } from './../store/useAuth';
-import { useRouter } from 'next/navigation';
+import { useEffect, useState } from "react";
+import { useAuth } from "./../store/useAuth";
+import { useRouter } from "next/navigation";
 
 export const Authentication = () => {
-  const {user, signIn, signOut} = useAuth();
+  const { user, signIn, signOut } = useAuth();
   const router = useRouter();
   useEffect(() => {
-    const loggedInUser = JSON.parse(localStorage.getItem('user'));
+    const loggedInUser = JSON.parse(localStorage.getItem("user"));
     if (!loggedInUser) return;
     signIn(loggedInUser);
   }, []);
@@ -16,18 +16,16 @@ export const Authentication = () => {
   return (
     <>
       {user && (
-          <button
-            className='text-white z-[100] w-15 absolute top-[2%] right-[2%]  bg-gray-900/50 p-3 rounded-md'
-            onClick={() => {
-              signOut();
-              // localStorage.removeItem('user');
-              router.push('/');
-            }}
-          >
-            LogOut
-          </button>
+        <button
+          className="text-white z-[100] w-15 absolute top-[2%] right-[2%]  bg-gray-900/50 p-3 rounded-md"
+          onClick={() => {
+            signOut();
+            localStorage.removeItem("user");
+            router.push("/");
+          }}>
+          LogOut
+        </button>
       )}
-      
     </>
-  )
-}
+  );
+};
--- app/main/page.jsx
+++ app/main/page.jsx
@@ -1,49 +1,55 @@
-'use client'
-import { useState, useEffect, useRef } from 'react';
-import Movie from '../components/movie';
-import Link from 'next/link';
-import Header from './../components/header';
-import Nav from '../components/nav';
-import Genres from '../components/genres';
-import TopTen from '../components/topTen';
-import { useAuth } from './../store/useAuth';
-import { ArrowLeft, ArrowRight } from '../components/icons/icons';
+"use client";
+import { useState, useEffect, useRef } from "react";
+import Movie from "../components/movie";
+import Link from "next/link";
+import Header from "./../components/header";
+import Nav from "../components/nav";
+import Genres from "../components/genres";
+import TopTen from "../components/topTen";
+import { useAuth } from "./../store/useAuth";
 
 export default function Main() {
   const [movies, setMovies] = useState([]);
   const [headerImage, setHeaderImage] = useState([]);
   const { user } = useAuth();
-  
+
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-    accept: 'application/json',
-    Authorization: process.env.NEXT_PUBLIC_MOVIE_API_KEY
-    }
+      accept: "application/json",
+      Authorization: process.env.NEXT_PUBLIC_MOVIE_API_KEY,
+    },
   };
 
   useEffect(() => {
-    fetch('https://api.themoviedb.org/3/discover/movie?certification_country=asia%2Cus&include_adult=false&include_video=true&language=ko-KO&page=10&region=asia%2Cus&sort_by=popularity.desc', options)
-    .then(response => response.json())
-    .then((response) => {
-    setMovies(response.results);
-    setHeaderImage(response.results[Math.floor(Math.random() * response.results.length - 1)]);
-    })
-    .catch(err => console.error(err));
-    }, []);
-
-  if(!user){
-    return(
-      <div>
-        <Link href={'/'}>로그인</Link>을 해주세요
-      </div>
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?certification_country=asia%2Cus&include_adult=false&include_video=true&language=ko-KO&page=10&region=asia%2Cus&sort_by=popularity.desc",
+      options
     )
+      .then((response) => response.json())
+      .then((response) => {
+        setMovies(response.results);
+        setHeaderImage(
+          response.results[
+            Math.floor(Math.random() * response.results.length - 1)
+          ]
+        );
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!user) {
+    return (
+      <div>
+        <Link href={"/"}>로그인</Link>을 해주세요
+      </div>
+    );
   }
 
   return (
-    <div className='w-full h-full font-RobotoMono'>
+    <div className="w-full h-full font-RobotoMono">
       <Nav />
-      <div className='w-full'>
+      <div className="w-full">
         <Link key={headerImage.id} href={`detail/${headerImage.id}`}>
           <Header
             key={headerImage.id}
@@ -51,51 +57,55 @@ export default function Main() {
             title={headerImage.title}
             headerImage={headerImage.backdrop_path}
           />
-          <div className='text-black z-[100] absolute top-[65%] left-[25%] w-1/2 bg-gray-400/50 p-3 rounded-md mx-auto text-center'>
-            <p className='font-extrabold text-xl mb-2.5 border-b-solid border-b-white border-b-2 pb-0.5 w-80 mx-auto'>{headerImage.title}</p>
-            <p className='font-semibold line-clamp-3 m-10'>{headerImage.overview ? headerImage.overview : '설명이 없습니다.'}</p>
+          <div className="text-black z-[100] absolute top-[65%] left-[25%] w-1/2 bg-gray-400/50 p-3 rounded-md mx-auto text-center">
+            <p className="font-extrabold text-xl mb-2.5 border-b-solid border-b-white border-b-2 pb-0.5 w-80 mx-auto">
+              {headerImage.title}
+            </p>
+            <p className="font-semibold line-clamp-3 m-10">
+              {headerImage.overview ? headerImage.overview : "설명이 없습니다."}
+            </p>
           </div>
         </Link>
       </div>
 
-      <div className='translate-y-[-2%] overflow-x-hidden max-w-screen-3xl relative'>
-        <div className='mb-5'>
-          <h1 className='ml-2.5 text-3xl font-bold mb-1.5 bg-gray-600/50 p-3 rounded-md w-[15%]'>지금 뜨는 콘텐츠</h1>
-          <ul className='flex justify-between relative flex-wrap'>
-            {movies.map((movie) => {
-              return(
-                <li key={movie.id} className='mb-7 ml-2.5'>
-                  <Link key={movie.id} href={`detail/${movie.id}`}>
-                    <Movie
-                      key={movie.id}
-                      id={movie.id}
-                      posterImg={movie.backdrop_path}
-                      title={movie.title}
-                    />
-                  </Link>
-                </li>
-              )
-            }).sort((a, b) => b.vote_average - a.vote_average).slice(0, 10)}
+      <div className="translate-y-[-2%] overflow-x-hidden max-w-screen-3xl relative">
+        <div className="mb-5">
+          <h1 className="ml-2.5 text-3xl font-bold mb-1.5 bg-gray-600/50 p-3 rounded-md w-[15%]">
+            지금 뜨는 콘텐츠
+          </h1>
+          <ul className="flex justify-between relative flex-wrap">
+            {movies
+              .map((movie) => {
+                return (
+                  <li key={movie.id} className="mb-7 ml-2.5">
+                    <Link key={movie.id} href={`detail/${movie.id}`}>
+                      <Movie
+                        key={movie.id}
+                        id={movie.id}
+                        posterImg={movie.backdrop_path}
+                        title={movie.title}
+                      />
+                    </Link>
+                  </li>
+                );
+              })
+              .sort((a, b) => b.vote_average - a.vote_average)
+              .slice(0, 10)}
           </ul>
         </div>
 
-        <div className='mb-7'>
-          <h1 className='ml-2.5 text-3xl font-bold mb-1.5'>인기 콘텐츠</h1>
-          <div className='flex justify-between relative mt-12'>
-            <TopTen
-              movies={movies}
-            />
-          </div>
-        </div>
-        
-        <div className='mt-28 mb-7'>
-          <div className='flex justify-between'>
-            <Genres
-              movies={movies}
-            />  
+        <div className="mb-7">
+          <h1 className="ml-2.5 text-3xl font-bold mb-1.5">인기 콘텐츠</h1>
+          <div className="flex justify-between relative mt-12">
+            <TopTen movies={movies} />
           </div>
         </div>
 
+        <div className="mt-28 mb-7">
+          <div className="flex justify-between">
+            <Genres movies={movies} />
+          </div>
+        </div>
       </div>
     </div>
   );
