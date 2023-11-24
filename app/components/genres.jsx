0472f2012713c875fb1da1e814f87385dfc21c5c
Dong-Han-Kim <dh32110@gmail.com>
2023-11-24 13:08:45 +0900

    장르 문제 드디어 해결!!

--- app/components/genres.jsx
+++ app/components/genres.jsx
@@ -20,36 +20,26 @@ export default function Genres({ movies }) {
       })
       .catch((err) => console.error(err));
   }, []);
-
+  console.log(genres);
   return (
     <div className="font-RobotoMono">
       {genres.map((genre) => {
-        const movieArray = [];
-        const categories = {
-          genreId: genre.id,
-          genreName: genre.name,
-          movieList: [],
-        };
         return (
-          <div key={genre.id}>
-            <h1 className="ml-2.5 mb-1.5 text-3xl font-bold">{genre.name}</h1>
+          <div key={genre.id} className={genre.name}>
+            <h1 className="ml-2.5 mb-1.5 text-3xl font-bold">
+              {document.querySelectorAll(`.${genre.name} .item`).length === 0
+                ? null
+                : genre.name}
+            </h1>
             <ul className="flex flex-wrap">
               {movies
                 .filter((movie) => {
-                  if (movie.genre_ids.includes(genre.id)) {
-                    categories.movieList.push({
-                      movieId: movie.id,
-                      movieTitle: movie.title,
-                    });
-                    return movieArray.push(categories);
-                  }
-                  return movieArray;
+                  return movie.genre_ids.includes(genre.id);
                 })
                 .map((movie) => {
-                  if (movieArray === 0) return;
                   return (
                     <Link href={`detail/${movie.id}`} key={movie.id}>
-                      <li>
+                      <li className="item">
                         <h1 className="ml-2.5 bg-gray-600/50 p-3 rounded-md">
                           {movie.title}
                         </h1>
