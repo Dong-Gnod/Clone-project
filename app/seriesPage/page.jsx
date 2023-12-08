46443e4a4786895a7d7f83f92c129c790e610a89
Dong-Han-Kim <dh32110@gmail.com>
2023-12-08 18:14:27 +0900

    wip

--- app/seriesPage/page.jsx
+++ app/seriesPage/page.jsx
@@ -6,6 +6,8 @@ import { fetchSeries } from "../util/http.js";
 import { useAuth } from "../store/useAuth";
 import Link from "next/link";
 import Nav from "../components/nav";
+import Series from "./../components/series";
+import Header from "../components/header";
 
 export default function SeriesPage() {
   const { user } = useAuth();
@@ -25,6 +27,7 @@ export default function SeriesPage() {
   console.log(data);
 
   const series = data.series.results;
+  const headerImage = series[Math.floor(Math.random() * series.length - 1)];
 
   if (!user) {
     return (
@@ -38,8 +41,33 @@ export default function SeriesPage() {
     <>
       <div>
         <Nav />
+        <div className="w-full flex justify-center">
+          <Link key={headerImage.id} href={`detail/${headerImage.id}`}>
+            <Header
+              key={headerImage.id}
+              id={headerImage.id}
+              title={headerImage.title}
+              headerImage={headerImage.backdrop_path}
+            />
+            <div className="text-black z-[100] absolute top-[65%] left-[25%] w-1/2 bg-gray-400/50 p-3 rounded-md mx-auto text-center">
+              <p className="font-extrabold text-xl mb-2.5 border-b-solid border-b-white border-b-2 pb-0.5 w-80 mx-auto">
+                {headerImage.name}
+              </p>
+              <p className="font-semibold line-clamp-3 m-10">
+                {headerImage.overview
+                  ? headerImage.overview
+                  : "설명이 없습니다."}
+              </p>
+            </div>
+          </Link>
+        </div>
       </div>
-      <Genres movies={series} />
+      <div>
+        <Series series={series} />
+      </div>
+      <div>
+        <Genres movies={series} />
+      </div>
     </>
   );
 }
