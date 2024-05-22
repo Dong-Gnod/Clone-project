3c63a6f77ea793c061b4063ad2e52a6a101f13cc
Dong-Han-Kim <dh32110@gmail.com>
2024-05-23 04:50:57 +0900

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		'./Components/**/*.{js,ts,jsx,tsx}',
-		'./src/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/**/*.{js,jsx,ts,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./public/index.html',
 	],
 	theme: {
 		extend: {
